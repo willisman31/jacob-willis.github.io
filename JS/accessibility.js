2629ef98529eb2527176867f6a3e19446277f770
willisman31 <58826505+willisman31@users.noreply.github.com>
2022-08-27 15:33:47 -0400

    Create accessibility.js

---
+++ JS/accessibility.js
@@ -0,0 +1,22 @@
+function setLightMode() {
+    document.getElementByTagName(body).style.background = "black";
+    document.getElementByTagName(body).style.color = "white";
+}
+
+function setDarkMode() {
+    document.getElementByTagName(body).style.background = "black";
+    document.getElementByTagName(body).style.color = "white";
+}
+
+function setDefaultMode() {
+    document.getElementByTagName(body).style.color = "black";
+    document.getElementByClass(content).style.color = "chartreuse";
+    document.getElementByTagName(h1).style.color = "aqua";
+    document.getElementByTagName(h2).style.color = "aqua";
+    document.getElementByTagName(h3).style.color = "aqua";
+    document.getElementByTagName(h4).style.color = "aqua";
+    document.getElementByTagName(h5).style.color = "aqua";
+    document.getElementByTagName(h6).style.color = "aqua";
+    document.getElementByTagName(a).style.color = "aqua";
+}
+
